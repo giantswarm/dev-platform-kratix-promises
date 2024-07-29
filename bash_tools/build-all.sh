a74db7dd83f02972a6ce333f9a3c62329e86fc90
Łukasz Piątkowski <piontec@gmail.com>
2024-07-29 13:29:28 +0200

    fix building with new config and with circleci

--- bash_tools/build-all.sh
+++ bash_tools/build-all.sh
@@ -4,13 +4,10 @@ my_dir=$(dirname -- "$(readlink -f -- "$0")")
 source "$my_dir/../build-config.sh"
 
 for dir in containers/*; do
-	if [[ ! -d "$dir" || ! -f "$dir/Dockerfile" ]]; then
-		continue
-	fi
-	image_name="$images_registry/$(basename "$dir")"
-	echo "Building $image_name..."
-	(
-		cd "$dir"
-		docker build --build-context shared=../shared -t "$image_name" .
-	)
+  if [[ ! -d "$dir" || ! -f "$dir/Dockerfile" ]]; then
+    continue
+  fi
+  image_name="$images_registry/$(basename "$dir")"
+  echo "Building $image_name..."
+  docker build -f "$dir/Dockerfile" -t "$image_name" containers
 done
