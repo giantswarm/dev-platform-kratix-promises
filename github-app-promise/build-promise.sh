1a3db5e51b345c407d3096483537914cbdc24892
Łukasz Piątkowski <piontec@gmail.com>
2024-07-31 09:45:15 +0200

    build promise

--- github-app-promise/build-promise.sh
+++ github-app-promise/build-promise.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yq -i '.spec.api.spec.versions.[0].schema.openAPIV3Schema.properties.spec.properties.appDeployment = (
-  load("../app-deployment-promise/promise.yaml").spec.api.spec.versions.[] | select (.name == "v1beta1") | .schema.openAPIV3Schema.properties.spec | del(.properties.statusConfigMapReference) | del(.required.[] | select(. == "statusConfigMapReference"))
+  load("../app-deployment-promise/promise.yaml").spec.api.spec.versions.[] 
+    | select (.name == "v1beta1") | .schema.openAPIV3Schema.properties.spec | del(.properties.statusConfigMapReference) | del(.required.[] | select(. == "statusConfigMapReference"))
 )' promise.yaml
 yq -i '.spec.api.spec.versions.[0].schema.openAPIV3Schema.properties.spec.properties.githubRepo = (load("../github-template-repo-promise/promise.yaml").spec.api.spec.versions.[] | select (.name == "v1beta1") | .schema.openAPIV3Schema.properties.spec)' promise.yaml
